6a1d17f8406b5f72fc081797fda08e06a1416375
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-05-02 17:37:20 +0430

    changed Controls settings, #13 issues

---
+++ Assets/Input/Controls.cs
@@ -0,0 +1,266 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""SpaceShip"",
+            ""id"": ""abaa505f-76cd-4a34-a955-f1f5104b4971"",
+            ""actions"": [
+                {
+                    ""name"": ""Steering_keyboard"",
+                    ""type"": ""Value"",
+                    ""id"": ""9b778250-5616-46d7-a87c-10921c128287"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Steering_mouse"",
+                    ""type"": ""Value"",
+                    ""id"": ""5ac58c89-e668-48f7-9a08-4554be5d4ea8"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""a23f3d89-f2a3-49bd-9c19-08addcecadcc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Steering_keyboard"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c871cc39-c337-4cb5-8cdf-0c5bb7ec98c0"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Steering_keyboard"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""62bbcfe4-b2d5-4e80-8ea2-277dbd8e4645"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Steering_keyboard"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""8b07334c-107c-40ab-b91b-b6d8ecaa7ee5"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Steering_keyboard"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""4053081d-ddcf-4374-ba97-48e2304c9332"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Steering_keyboard"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0c540d58-ae8c-4873-8a6b-1980354c1027"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse"",
+                    ""action"": ""Steering_mouse"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Mouse"",
+            ""bindingGroup"": ""Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": true,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // SpaceShip
+        m_SpaceShip = asset.FindActionMap("SpaceShip", throwIfNotFound: true);
+        m_SpaceShip_Steering_keyboard = m_SpaceShip.FindAction("Steering_keyboard", throwIfNotFound: true);
+        m_SpaceShip_Steering_mouse = m_SpaceShip.FindAction("Steering_mouse", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // SpaceShip
+    private readonly InputActionMap m_SpaceShip;
+    private ISpaceShipActions m_SpaceShipActionsCallbackInterface;
+    private readonly InputAction m_SpaceShip_Steering_keyboard;
+    private readonly InputAction m_SpaceShip_Steering_mouse;
+    public struct SpaceShipActions
+    {
+        private @Controls m_Wrapper;
+        public SpaceShipActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Steering_keyboard => m_Wrapper.m_SpaceShip_Steering_keyboard;
+        public InputAction @Steering_mouse => m_Wrapper.m_SpaceShip_Steering_mouse;
+        public InputActionMap Get() { return m_Wrapper.m_SpaceShip; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(SpaceShipActions set) { return set.Get(); }
+        public void SetCallbacks(ISpaceShipActions instance)
+        {
+            if (m_Wrapper.m_SpaceShipActionsCallbackInterface != null)
+            {
+                @Steering_keyboard.started -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_keyboard;
+                @Steering_keyboard.performed -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_keyboard;
+                @Steering_keyboard.canceled -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_keyboard;
+                @Steering_mouse.started -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_mouse;
+                @Steering_mouse.performed -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_mouse;
+                @Steering_mouse.canceled -= m_Wrapper.m_SpaceShipActionsCallbackInterface.OnSteering_mouse;
+            }
+            m_Wrapper.m_SpaceShipActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Steering_keyboard.started += instance.OnSteering_keyboard;
+                @Steering_keyboard.performed += instance.OnSteering_keyboard;
+                @Steering_keyboard.canceled += instance.OnSteering_keyboard;
+                @Steering_mouse.started += instance.OnSteering_mouse;
+                @Steering_mouse.performed += instance.OnSteering_mouse;
+                @Steering_mouse.canceled += instance.OnSteering_mouse;
+            }
+        }
+    }
+    public SpaceShipActions @SpaceShip => new SpaceShipActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    private int m_MouseSchemeIndex = -1;
+    public InputControlScheme MouseScheme
+    {
+        get
+        {
+            if (m_MouseSchemeIndex == -1) m_MouseSchemeIndex = asset.FindControlSchemeIndex("Mouse");
+            return asset.controlSchemes[m_MouseSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface ISpaceShipActions
+    {
+        void OnSteering_keyboard(InputAction.CallbackContext context);
+        void OnSteering_mouse(InputAction.CallbackContext context);
+    }
+}
