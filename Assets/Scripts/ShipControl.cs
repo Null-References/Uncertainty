745cf9bf66fc8824a82c19f6ec9fe377a7d69cc8
mohmehdi <m.m.afkhami@outlook.com>
2021-05-16 19:08:16 +0430

    mouse control value fixed #14 issue

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -7,17 +7,22 @@ public class ShipControl : MonoBehaviour
 { 
     
     [SerializeField] private float moveSpeed = 1f;
-    
-    
+    [Range(0,1)]
+    [SerializeField] private float mouseDeadZone = 0.6f;
+    [SerializeField] private float deadZoneCoef = 5;
+
+    private Vector2 testVector;
+    private Vector3 controlMousePos;
+    private Vector3 controlMousePosAfter;
+
     private Rigidbody rb = null;
     private Controls controls;
-
-
+    
     private void Awake()
     {
         controls = new Controls();
         
-        controls.SpaceShip.Rotation.performed += ctx => Move(ctx.ReadValue<Vector2>());
+       // controls.SpaceShip.Rotation.performed += ctx => Move(ctx.ReadValue<Vector2>());
        
     }
 
@@ -36,7 +41,7 @@ public class ShipControl : MonoBehaviour
         controls.Disable();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Move(Vector2.zero);
     }
@@ -44,9 +49,30 @@ public class ShipControl : MonoBehaviour
     private void Move(Vector2 dir)
     {
 
-        Vector2 pos = controls.SpaceShip.Rotation.ReadValue<Vector2>();
-        rb.AddForce(new Vector3(pos.x, 0, pos.y) * moveSpeed);
-        Debug.Log(dir.ToString());
+        Vector2 mouseVec = controls.SpaceShip.Steering_mouse.ReadValue<Vector2>();
+        mouseVec = new Vector2(mouseVec.x- (Screen.width/2), mouseVec.y- (Screen.height/2)) /(Screen.width / 2);
+
+       
+        if (mouseVec.magnitude > mouseDeadZone)
+        {
+            mouseVec = mouseVec.normalized * mouseDeadZone;
+        }
+
+        testVector = new Vector3(mouseVec.x, mouseVec.y, 0)- transform.position;;
+        Debug.Log(mouseVec * deadZoneCoef );
+        // rb.AddForce(new Vector3(pos.x, 0, pos.y) * moveSpeed);
+        //Debug.Log(1/Mathf.Pow(pos,2));
     }
 
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawRay(transform.position ,new Vector3( -testVector.x ,testVector.y,0)*deadZoneCoef);
+        
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawSphere(transform.position + controlMousePos, 0.1f);
+
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawWireSphere(transform.position + controlMousePosAfter, 0.1f);
+    //}
 }
