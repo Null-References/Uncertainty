f864de1beaea6913e2315129be2ac6382cbc657a
mohmehdi <m.m.afkhami@outlook.com>
2021-06-08 00:41:07 +0430

    implemented base of timers #23

---
+++ Assets/Scripts/Timer/CustomTimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public abstract class CustomTimer
+{
+    protected float Duration { get; set; }
+
+    protected float _remainingTime;
+
+    protected CustomTimer(float duration)
+    {
+        Duration = duration;
+       _remainingTime = duration;
+    }
+
+    public abstract void Tick(float deltaTime);
+
+}
+
