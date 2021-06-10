6b04563d7927b94577d01db6e6d7aeb46b45c7b0
mohmehdi <m.m.afkhami@outlook.com>
2021-06-10 13:01:20 +0430

    aiming raycast added , shooting system almost done #23

--- Assets/Scripts/ShipShootingControler.cs
+++ Assets/Scripts/ShipShootingControler.cs
@@ -1,37 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+/// <summary>
+/// gets input and aims the gun
+/// </summary>
+[RequireComponent(typeof(ShipInputValueHandler))]
 public class ShipShootingControler : MonoBehaviour
 {
-
+    [Tooltip("Which layers we can aim at")]
+    [SerializeField] LayerMask aimableLayer;
+    [SerializeField] private Camera shipCamera;
+    [Header("Ship Weapon")]
     [SerializeField] private WeaponBase weapon;
+    [Tooltip("transform of the shooting point of the weapon")]
+    [SerializeField] Transform shootPoint;
+    
+    [Header("Aiming")]
+    [SerializeField] float aimRaduis = 100;
+    [SerializeField] float aimMinDistance = 5f;    
 
-    [SerializeField] private Camera cam;
-    [SerializeField] Transform shopPoint;
-    [SerializeField] float aimCircleBiggness = 100;
     private ShipInputValueHandler _inputHandler;
+    private Vector3 _screenSpaceAimPoint;
     private void Start()
     {
         _inputHandler = GetComponent<ShipInputValueHandler>();
     }
     private void Update()
     {
-        Vector3 v = Input.mousePosition;
-        Vector2 dist = v- new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        Vector2 result = dist;
-        if (dist.magnitude>aimCircleBiggness)
-        {
-            result = dist * (aimCircleBiggness / dist.magnitude);
-        }
-        Debug.Log($"{result.magnitude} ");
-        var aimPoint =cam.ScreenToWorldPoint(new Vector3 (Screen.width / 2 +result.x, Screen.height / 2+result.y,50));
+        CalculateScreenSpaceAimPoint();
+        var worldSpaceAimPoint = shipCamera.ScreenToWorldPoint(_screenSpaceAimPoint);
+        GetRaycastHitPoint(ref worldSpaceAimPoint);
 
-        shopPoint.LookAt(aimPoint);
+        shootPoint.LookAt(worldSpaceAimPoint);
 
-        if (_inputHandler.GetShootInput()) 
+        if (_inputHandler.GetShootInput())
         {
             weapon.Shoot();
         }
+    }
+
+    private void GetRaycastHitPoint(ref Vector3 worldSpaceAimPoint)
+    {
+        RaycastHit raycastHit;
+        Vector3 direction = worldSpaceAimPoint - shootPoint.position;
+        if (Physics.Raycast(shipCamera.transform.position, direction, out raycastHit, 50, aimableLayer))
+        {
+            if (Vector3.Distance(raycastHit.point, shootPoint.position) > aimMinDistance)
+            {
+                worldSpaceAimPoint = raycastHit.point;
+            }
+        }
+        //Debug.DrawLine(shootPoint.position, worldSpaceAimPoint, Color.red);
+    }
+
+    private void CalculateScreenSpaceAimPoint()
+    {
+        //mouse pos in screen space
+        Vector3 mousePos = Input.mousePosition;
+        //deffrence vector from center to mousePos, pointing at mousePos
+        Vector2 diffrence = mousePos - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+
+        Vector2 result = diffrence;
+        //if lenght of diff gone over raduis we set it lenght to be inside circle
+        float diffrenceLenght = diffrence.magnitude;
+        if (diffrenceLenght > aimRaduis)
+            result = diffrence * (aimRaduis / diffrenceLenght);
+
+        //centerize aimsight vector
+        _screenSpaceAimPoint = new Vector3(Screen.width / 2 + result.x, Screen.height / 2 + result.y, 50);
     }
 }
