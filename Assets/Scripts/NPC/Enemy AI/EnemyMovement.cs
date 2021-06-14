5a945dcf6a60c290ae33e7a230295c794c64c94e
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-06-15 00:29:29 +0430

    our baby Bee took her first step :") (idle movement), #24 #25

--- Assets/Scripts/NPC/Enemy AI/EnemyMovement.cs
+++ Assets/Scripts/NPC/Enemy AI/EnemyMovement.cs
@@ -1,35 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private List<Transform> pathPoints;
+    [SerializeField] private float timePeriod = 15f;
+    [Range(0,1)]
+    [SerializeField] private float smoothness = 0.1f;
+    [SerializeField] private Animator animator;
 
     private Transform _player;
+    private RepeatableTimer _timer;
+    private Transform _currentPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameManager.Instance.GetPlayerTransform();
+        _currentPoint = transform;
+        _timer = new RepeatableTimer(timePeriod);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(_player);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        _timer.Tick(Time.deltaTime);
+        Idle();
     }
+
     private void Idle()
     {
+        SetIdleAnimation();
+        if (_timer.IsReady())
+        {
+            var pointCount = pathPoints.Count;
+            var rand = Random.Range(0, pointCount - 1);
+            var selectedPoint = pathPoints[rand];
+            _currentPoint = selectedPoint;
+        }
 
+        transform.rotation = Quaternion.Lerp(transform.rotation, _currentPoint.rotation, smoothness);
     }
+
+    private void SetIdleAnimation()
+    {
+        animator.SetBool("Shoot", false);
+    }
+
     private void Patrol()
     {
-
     }
+
     private void TargetPlayer()
     {
-
     }
 }
