5061329bfc2513ac7caf7e03d3c78a7af9747f43
mohmehdi <m.m.afkhami@outlook.com>
2021-06-14 22:37:31 +0430

    added gamemanager  , enemy move and shoot scripts #25 #24

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+        //DontDestroyOnLoad(this.gameObject);
+    }
+    public Transform GetPlayerTransform() => playerTransform;
+}
---
+++ Assets/Scripts/NPC/Enemy AI/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Transform _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameManager.Instance.GetPlayerTransform();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_player);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    private void Idle()
+    {
+
+    }
+    private void Patrol()
+    {
+
+    }
+    private void TargetPlayer()
+    {
+
+    }
+}
