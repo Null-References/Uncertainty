24bcca904bc8a6b33424d77be3996aa8727ac2db
mohmehdi <m.m.afkhami@outlook.com>
2021-06-16 03:24:23 +0430

    added a condition and lookat for boss #25 #28

---
+++ Assets/Scripts/NPC/Enemy AI/LookAt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [Range(0,180)]
+    [SerializeField] private float radius = 30f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothness=0.1f;
+
+    private Vector3 _defaultForward;
+
+    private void Start()
+    {
+        _defaultForward = transform.forward;
+        if (!target)
+        {
+            Debug.Log($"Set target for Look at component On: {gameObject.name} ");
+        }
+    }
+    private void Update()
+    {
+        if (!target) return;
+
+        if (Vector3.Angle(target.position - transform.position, _defaultForward) < radius)
+        {
+            var direction = (target.position - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, direction, smoothness);
+        }
+        else
+            transform.forward = Vector3.Lerp(transform.forward, _defaultForward, smoothness);
+    }
+}
---
+++ Assets/Scripts/NPC/Enemy AI/StateMachine/Condition/TimeExceeded.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TimeExceeded : Condition
+{
+    [SerializeField] private float duration = 1f;
+
+    private float _lastTime = 0;
+    public override bool IsMet()
+    {
+        if (Time.time - _lastTime > duration )
+        {
+            _lastTime = Time.time;
+            return true;
+        }
+        return false;
+    }
+}
