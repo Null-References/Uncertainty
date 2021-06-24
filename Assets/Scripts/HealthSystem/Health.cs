d5e730ee5240be2ab5ce99d8e91f9ba1cd7a57ec
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-06-24 19:48:36 +0430

    Analyzed the architecture we're going to implement with Mohmehdi, #40

---
+++ Assets/Scripts/HealthSystem/Health.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+
+    private float _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = maxHealth;
+        
+    }
+    
+    
+    
+}
---
+++ Assets/Scripts/Weapons/IProjectile.cs
@@ -0,0 +1,7 @@
+﻿namespace Weapons
+{
+    public interface IProjectile
+    {
+        
+    }
+}
