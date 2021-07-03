f0ccacda76357d4e74164737a2cb779c609e9b86
mohmehdi <m.m.afkhami@outlook.com>
2021-07-03 06:42:34 +0430

    wasp  and wasp_pool added , queen can spawn and set bots properties changed enemy movment classes for a null refrence problem #28

---
+++ Assets/Scripts/NPC/QueenWaspSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class QueenWaspSpawner : MonoBehaviour
+{
+    [SerializeField] private int difficulty = 1;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private int maxAliveBots = 5;
+    [SerializeField] private float spawnRate = 10f;
+    [SerializeField] private List<Transform> pathPoints;
+
+    private RepeatableTimer _timer;
+    private int _currentBotCount = 0;
+    private void Start()
+    {
+        _timer = new RepeatableTimer(spawnRate);
+    }
+
+    private void Update()
+    {
+        _timer.Tick(Time.deltaTime);
+        if (_currentBotCount < maxAliveBots)
+        {
+            if (_timer.IsReady())
+            {
+                var newBot = WaspBotPool.Instance.Get();
+                newBot.gameObject.SetActive(true);
+                newBot.transform.position = spawnPosition.position;
+                newBot.SetPathPoints(pathPoints);
+                _currentBotCount++;
+            }
+        }
+    }
+}
