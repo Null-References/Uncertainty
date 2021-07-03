65be7d04037e704de2d8c61ec57b5781915c4cd9
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-07-03 15:51:41 +0430

    Added base of save/load system #49

---
+++ Assets/Scripts/SavingAndLoading/SavingLoadingData.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SavingLoadingData : MonoBehaviour
+{
+    public static void Save<T>(T saveThisThing, string saveName)
+    {
+        string path = Application.persistentDataPath + "/Uncertainty/saves/";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (FileStream fileStream = new FileStream(path + saveName + ".json", FileMode.Create))
+        {
+            binaryFormatter.Serialize(fileStream, saveThisThing);
+        }
+    }
+
+    public static T Load<T>(string saveName)
+    {
+        T getThisThing;
+        string path = Application.persistentDataPath + "/Uncertainty/saves/";
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (FileStream fileStream = new FileStream(path + saveName + ".json", FileMode.Open))
+        {
+            getThisThing = (T) binaryFormatter.Deserialize(fileStream);
+        }
+
+        return getThisThing;
+    }
+
+    public static bool SaveExist(string saveName)
+    {
+        string saveFile = Application.persistentDataPath + "/Uncertainty/saves/" + saveName + ".json";
+        return File.Exists(saveFile);
+    }
+
+    public static void DeleteSave(string saveName)
+    {
+        string saveFile = Application.persistentDataPath + "/Uncertainty/saves/" + saveName + ".json";
+        File.Delete(saveFile);
+    }
+}
